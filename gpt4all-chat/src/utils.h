452dc28eef4ad6b16d65ffed8f453cc47b08a35b
Shoshan Dagany <shoshan@gmail.com>
2024-10-18 15:21:09 +0300

    utils.h: solve MAKE_FORMATTER

--- gpt4all-chat/src/utils.h
+++ gpt4all-chat/src/utils.h
@@ -11,15 +11,21 @@
 
 // fmtlib formatters for QString and QVariant
 
-#define MAKE_FORMATTER(type, conversion)                                      \
-    template <>                                                               \
-    struct fmt::formatter<type, char>: fmt::formatter<std::string, char> {    \
-        template <typename FmtContext>                                        \
-        FmtContext::iterator format(const type &value, FmtContext &ctx) const \
-        {                                                                     \
-            return formatter<std::string, char>::format(conversion, ctx);     \
-        }                                                                     \
+#define MAKE_FORMATTER(type, conversion)                                       \
+    template <>                                                                \
+    struct fmt::formatter<type, char> : fmt::formatter<std::string, char> {    \
+        template <typename FmtContext>                                         \
+        typename FmtContext::iterator format(const type &value, FmtContext &ctx) const \
+        {                                                                      \
+            return formatter<std::string, char>::format(conversion(value), ctx); \
+        }                                                                      \
     }
 
-MAKE_FORMATTER(QString,  value.toStdString()           );
-MAKE_FORMATTER(QVariant, value.toString().toStdString());
+auto QStringConversion = [](const QString &value) { return value.toStdString(); };
+auto QVariantConversion = [](const QVariant &value) { return value.toString().toStdString(); };
+
+MAKE_FORMATTER(QString,  QStringConversion);
+MAKE_FORMATTER(QVariant, QVariantConversion);
+
+
+
